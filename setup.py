7141cec0a0adccf15460673c7262b6c50bea4192
Jke <jke.contact.me@gmail.com>
2025-06-12 02:12:08 +0100

    Include custom component in package

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.6',
     description='Device management utilities with Home Assistant integration',
     packages=find_packages(),
+    py_modules=["womgr_cli", "dashboard_cli"],
     include_package_data=True,
-    install_requires=["requests"],
+    install_requires=["requests", "voluptuous"],
 )
